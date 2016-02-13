f5724d5d5c33fd2733376ac8d3ebfb8bad85df67
Jordi Kroon <jordi@jordikroon.nl>
2016-02-13 20:03:42 +0100

    Fixed some bugs, prepare for heroku

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once '../autoload.php';
-require_once '../config/autoload/autoload_namespaces.php';
+require_once '../Config/Autoload/autoload_namespaces.php';
 
 require_once '../Vendor/autoload.php';
 
@@ -13,11 +13,10 @@ require_once '../Vendor/autoload.php';
 \***************************************************************************/
 
 if (isset($namespaces) && is_array($namespaces)) {
-	foreach ($namespaces AS $namespaces) {
-		$classLoader = new ClassLoader($namespaces);
+	foreach ($namespaces AS $namespace) {
+		$classLoader = new ClassLoader($namespace);
 		$classLoader -> register();
 	}
 }
-
 
 echo \System\Framework\Application::run();
