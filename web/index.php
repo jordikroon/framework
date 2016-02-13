5ac633a0ea56148aec4567d3c6b8c233ffae06b8
Jordi Kroon <jordi@jordikroon.nl>
2016-02-13 22:30:34 +0100

    Use DIR

--- web/index.php
+++ web/index.php
@@ -3,6 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once '../Vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 echo \System\Framework\Application::run();
