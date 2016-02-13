3b0ee7619a929805b6cd72d012507851d31cde78
Jordi Kroon <jordi@jordikroon.nl>
2016-02-13 20:16:31 +0100

    Cleanup code

--- web/index.php
+++ web/index.php
@@ -3,20 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once '../autoload.php';
-require_once '../Config/Autoload/autoload_namespaces.php';
-
 require_once '../Vendor/autoload.php';
-
-/***************************************************************************\
- *                                INIT APPLICATION                         *
-\***************************************************************************/
-
-if (isset($namespaces) && is_array($namespaces)) {
-	foreach ($namespaces AS $namespace) {
-		$classLoader = new ClassLoader($namespace);
-		$classLoader -> register();
-	}
-}
 
 echo \System\Framework\Application::run();
